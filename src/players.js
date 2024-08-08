3e786861a5fade6e4b2cac21f336adb5c3cacc1f
Richie77777777 <richeanyadike7@gmail.com>
2024-08-08 12:26:36 +0100

    first commit

---
+++ src/players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Inter Miami",
+      nationality: "Argentina",
+      jerseyNumber: 10,
+      age: 36,
+      imageUrl: "https://cdn.vox-cdn.com/thumbor/Y3Kr3A9MQDMuZfFIpKG6KjXa9SA=/0x0:3000x2000/1200x800/filters:focal(1160x657:1640x1137)/cdn.vox-cdn.com/uploads/chorus_image/image/73394029/2155658434.0.jpg"
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "Al-Nassr",
+      nationality: "Portugal",
+      jerseyNumber: 7,
+      age: 39,
+      imageUrl: "https://phantom-marca.unidadeditorial.es/23b32a9cdc094cf50cbce2fa57c977cd/resize/828/f/jpg/assets/multimedia/imagenes/2022/12/05/16702399461500.jpg"
+    },
+    {
+        name: "Neymar",
+        team: "Al-Hilal",
+        nationality: "Brazil",
+        jerseyNumber: 10,
+        age: 31,
+        imageUrl: "https://images.gmanews.tv/webpics/2023/08/Thumb_size_1200x630_(32)_2023_08_16_17_59_00.jpg"
+      },
+      {
+        name: "Mohammed Salah",
+        team: "Liverpool",
+        nationality: "Egypt",
+        jerseyNumber: 11,
+        age: 32,
+        imageUrl: "https://tmssl.akamaized.net/images/foto/galerie/salah-mohamed-liverpool-1521366000-14513.jpg?lm=1521366010"
+      },
+    
+  ];
+  
+  export default players;
